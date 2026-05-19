d8581f6a7479f9b06d973677692b1dcbb51b1ee7
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2026-05-19 16:20:14 -0300

    fix api base runtimeconfig in nuxt.config

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/app/**': { ssr: false },
   },
-  
+
   css: [ '~/assets/css/main.css' ],
 
   vite: {
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_URL,
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
     },
   },
 
