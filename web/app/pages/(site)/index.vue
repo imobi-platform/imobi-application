d7450c4ca19c4af6caf80c25267b059392242124
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2026-05-18 23:32:29 -0300

    test automatic deploy

--- web/app/pages/(site)/index.vue
+++ web/app/pages/(site)/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center min-h-screen flex-col gap-4">
         <span class="text-2xl font-bold">Home do Site</span>
-        <button @click="navigateTo('/app')" class="bg-orange-500">Ir para o Painel</button>
+        <button @click="navigateTo('/app')" class="bg-orange-500 p-10">Ir para o Painel</button>
     </div>
 </template>
